784e054c6f9ee625d8dbaf23356877b761437bbc
pavlo-ivashchenko <pavlo.ivashchenko@allianz.at>
2022-12-07 15:41:22 +0100

    ST-4: Fix typos errors

--- src/main/java/pavlo/pro/massage/therapy/api/Application.java
+++ src/main/java/pavlo/pro/massage/therapy/api/Application.java
@@ -1,4 +1,4 @@
-package pavlo.pro.massage.therapy.api.service;
+package pavlo.pro.massage.therapy.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,9 +7,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @SpringBootApplication
 @EnableMongoRepositories
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
