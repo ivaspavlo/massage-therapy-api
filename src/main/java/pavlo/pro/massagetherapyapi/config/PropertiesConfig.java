a4a641df764f68b06c4ab9dcc8755a02beb9a7b6
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2023-01-06 15:54:17 +0100

    ST-13.1: Fixing bug with PropertyConfig

--- src/main/java/pavlo/pro/massagetherapyapi/config/PropertiesConfig.java
+++ src/main/java/pavlo/pro/massagetherapyapi/config/PropertiesConfig.java
@@ -6,7 +6,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
-@PropertySource("classpath:application.custom.properties")
+@PropertySource("classpath:custom.properties")
 public class PropertiesConfig {
     @Autowired
     private Environment env;
