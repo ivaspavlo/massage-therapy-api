afc98f876b647f31a092dd333e42c8289173b4cd
pavlo-ivashchenko <pavlo.ivashchenko@allianz.at>
2023-01-21 14:51:54 +0100

    ST-19.2: Fix unit test for signup method

--- src/test/java/pavlo/pro/massagetherapyapi/unit/service/UserServiceImpTest.java
+++ src/test/java/pavlo/pro/massagetherapyapi/unit/service/UserServiceImpTest.java
@@ -1,11 +1,12 @@
 package pavlo.pro.massagetherapyapi.unit.service;
 
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.server.ResponseStatusException;
 import pavlo.pro.massagetherapyapi.dto.request.SignupReq;
 import pavlo.pro.massagetherapyapi.model.User;
@@ -15,12 +16,8 @@ import pavlo.pro.massagetherapyapi.service.impl.UserServiceImpl;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringRunner.class)
 public class UserServiceImpTest {
-
-    // https://blog.devgenius.io/how-to-code-in-java-spring-like-a-pro-dependency-injection-69249fdb68
-    // https://spring.io/guides/gs/testing-web/
-    // https://reflectoring.io/spring-boot-test/
 
     @Mock
     private UserRepository userRepository;
@@ -28,39 +25,34 @@ public class UserServiceImpTest {
     @InjectMocks
     private UserServiceImpl userServiceIpml;
 
-    @Test
-    public void whenSignup_shouldReturnUser() {
-        String name = "Test Name";
+    private String testEmail = "testEmail@mail.com";
+    private SignupReq testSignupReq = new SignupReq();
+    private User testUser = new User();
 
-        SignupReq signupReq = new SignupReq();
-        signupReq.setFirstName(name);
-
-        User user = new User();
-        user.setFirstName(name);
-
-        when(userRepository.save(ArgumentMatchers.any(User.class))).thenReturn(user);
-        User created = userServiceIpml.signup(signupReq);
-
-        assertThat(created.getFirstName()).isSameAs(user.getFirstName());
+    @BeforeEach
+    public void setupTestData() {
+        testSignupReq.setEmail(testEmail);
+        testUser.setEmail(testEmail);
     }
 
     @Test
-    public void whenSignup_shouldThrowResponseStatusException() {
-        String name = "Test Name";
+    public void whenSignup_shouldReturnUser() {
+        when(userRepository.save(ArgumentMatchers.any(User.class))).thenReturn(testUser);
+        User created = userServiceIpml.signup(testSignupReq);
+        assertThat(created.getFirstName()).isSameAs(testUser.getFirstName());
+    }
 
-        SignupReq signupReq = new SignupReq();
-        signupReq.setFirstName(name);
+    @Test
+    public void whenSignupWithTheSameEmail_shouldThrowResponseStatusException() {
+        when(userRepository.save(ArgumentMatchers.any(User.class))).thenReturn(testUser);
+        User created = userServiceIpml.signup(testSignupReq);
 
-        User user = new User();
-        user.setFirstName(name);
-
-        when(userRepository.save(ArgumentMatchers.any(User.class))).thenReturn(user);
-        User created = userServiceIpml.signup(signupReq);
+        when(userRepository.findByEmail(testUser.getEmail())).thenReturn(testUser);
 
         try {
-            userServiceIpml.signup(signupReq);
+            userServiceIpml.signup(testSignupReq);
         } catch (ResponseStatusException error) {
-
+            assertThat(error).isNotNull();
         }
     }
 
