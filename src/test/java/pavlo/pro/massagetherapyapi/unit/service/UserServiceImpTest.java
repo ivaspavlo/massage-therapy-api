2ed0706cb654a9fafe4c9e89b39b9cdb795c9b86
pavlo-ivashchenko <pavlo.ivashchenko@allianz.at>
2023-01-23 15:04:21 +0100

    ST-19.3: Adjust unit test to be fully isolated

--- src/test/java/pavlo/pro/massagetherapyapi/unit/service/UserServiceImpTest.java
+++ src/test/java/pavlo/pro/massagetherapyapi/unit/service/UserServiceImpTest.java
@@ -4,12 +4,15 @@ import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentMatchers;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.server.ResponseStatusException;
 import pavlo.pro.massagetherapyapi.dto.request.SignupReq;
+import pavlo.pro.massagetherapyapi.model.Role;
 import pavlo.pro.massagetherapyapi.model.User;
+import pavlo.pro.massagetherapyapi.model.enums.ERole;
+import pavlo.pro.massagetherapyapi.repository.RoleRepository;
 import pavlo.pro.massagetherapyapi.repository.UserRepository;
 import pavlo.pro.massagetherapyapi.service.impl.UserServiceImpl;
 
@@ -18,41 +21,56 @@ import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 public class UserServiceImpTest {
-
     @Mock
-    private UserRepository userRepository;
-
-    @InjectMocks
+    private PasswordEncoder mockPasswordEncoder;
+    @Mock
+    private RoleRepository mockRoleRepository;
+    @Mock
+    private UserRepository mockUserRepository;
     private UserServiceImpl userServiceIpml;
-
     private String testEmail = "testEmail@mail.com";
-    private SignupReq testSignupReq = new SignupReq();
-    private User testUser = new User();
+    private String testPwd = "testPwd";
+    private SignupReq mockSignupReq = new SignupReq(
+        testEmail,
+        testPwd,
+        "testFirstName",
+        "testLastName",
+        "+43111222333444"
+    );
 
     @BeforeEach
     public void setupTestData() {
-        testSignupReq.setEmail(testEmail);
-        testUser.setEmail(testEmail);
+        userServiceIpml = new UserServiceImpl(
+            mockPasswordEncoder,
+            mockRoleRepository,
+            mockUserRepository
+        );
+
+        User mockUser = new User();
+        mockUser.setEmail(testEmail);
+
+        Role mockRole = new Role();
+        mockRole.setName(ERole.ROLE_USER);
+
+        when(mockUserRepository.save(ArgumentMatchers.any(User.class))).thenReturn(mockUser);
+        when(mockUserRepository.findByEmail(testEmail)).thenReturn(mockUser);
+        when(mockRoleRepository.findByName(ERole.ROLE_USER.toString())).thenReturn(mockRole);
+        when(mockPasswordEncoder.encode(testPwd)).thenReturn(testPwd);
     }
 
     @Test
     public void whenSignup_shouldReturnUser() {
-        when(userRepository.save(ArgumentMatchers.any(User.class))).thenReturn(testUser);
-        User created = userServiceIpml.signup(testSignupReq);
-        assertThat(created.getFirstName()).isSameAs(testUser.getFirstName());
+        User created = userServiceIpml.signup(mockSignupReq);
+        assertThat(created).isInstanceOf(User.class);
     }
 
     @Test
     public void whenSignupWithTheSameEmail_shouldThrowResponseStatusException() {
-        when(userRepository.save(ArgumentMatchers.any(User.class))).thenReturn(testUser);
-        User created = userServiceIpml.signup(testSignupReq);
-
-        when(userRepository.findByEmail(testUser.getEmail())).thenReturn(testUser);
-
+        userServiceIpml.signup(mockSignupReq);
         try {
-            userServiceIpml.signup(testSignupReq);
+            userServiceIpml.signup(mockSignupReq);
         } catch (ResponseStatusException error) {
-            assertThat(error).isNotNull();
+            assertThat(error).isInstanceOf(ResponseStatusException.class);
         }
     }
 
