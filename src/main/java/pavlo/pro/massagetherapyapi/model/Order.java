2fbac670d687d2da6554dcee9147b2a75a20989a
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2023-01-24 17:11:42 +0100

    ST-20.1: Minor adjustment to existing booking models

--- src/main/java/pavlo/pro/massagetherapyapi/model/Order.java
+++ src/main/java/pavlo/pro/massagetherapyapi/model/Order.java
@@ -1,12 +1,20 @@
 package pavlo.pro.massagetherapyapi.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.validation.constraints.NotBlank;
 import java.util.HashSet;
 
-public class Order {
+@Data
+@NoArgsConstructor
+@Accessors(chain = true)
+@Document("massageOrders")
+public class MassageOrder {
     @Id
     private String id;
 
@@ -19,5 +27,5 @@ public class Order {
     String userId;
 
     @DBRef
-    HashSet<BookingSlot> items;
+    HashSet<MassageBookingSlot> items;
 }
