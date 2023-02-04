d7727e3cbee24e2e46bb9591000ab0c829fb2d09
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2023-02-04 17:30:08 +0100

    ST-31: Add logging for active profile

--- src/main/java/pavlo/pro/massagetherapyapi/MassageTherapyApiApplication.java
+++ src/main/java/pavlo/pro/massagetherapyapi/MassageTherapyApiApplication.java
@@ -1,5 +1,7 @@
 package pavlo.pro.massagetherapyapi;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,11 +9,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableEncryptableProperties
 public class MassageTherapyApiApplication {
 
+	@Value("${spring.profiles.active}")
+	private String activeProfile;
+
 	public static void main(String[] args) {
 		SpringApplication.run(MassageTherapyApiApplication.class, args);
 	}
 
 }
+
+// Terminal command
+// mvn spring-boot:run -Dspring-boot.run.profiles=dev
 
 // Jasypt instruction
 // https://medium.com/developervisits/hiding-encrypting-database-password-in-the-application-properties-34d59fe104eb
