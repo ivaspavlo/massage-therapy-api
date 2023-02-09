7b08dbf7e8cd6eb949ac473002eb56545c4027ba
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2023-02-09 21:42:58 +0100

    ST-34.2: Fix bug with active profile

--- src/main/java/pavlo/pro/massagetherapyapi/config/AppConfig.java
+++ src/main/java/pavlo/pro/massagetherapyapi/config/AppConfig.java
@@ -1,7 +1,6 @@
 package pavlo.pro.massagetherapyapi.config;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,18 +9,14 @@ import javax.annotation.PostConstruct;
 @Configuration
 public class AppConfig {
 
-    @Value("${spring.profiles.active}")
-    private String activeProfiles;
-
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
 
-    @PostConstruct
-    public void printConfigValues() {
-        System.out.println("**** APPLICATION ACTIVE PROFILE ****");
-        System.out.println(activeProfiles);
+    @Bean
+    public AppEnvironmentAware myEnvironmentAware() {
+        return new AppEnvironmentAware();
     }
 
 }
