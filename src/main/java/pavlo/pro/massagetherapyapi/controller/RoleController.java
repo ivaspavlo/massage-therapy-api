60708a3a56cec8754cb8346dd1b5b0aeab13a482
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2023-02-15 13:39:31 +0100

    ST-39: Implement get all roles functionality

--- src/main/java/pavlo/pro/massagetherapyapi/controller/RoleController.java
+++ src/main/java/pavlo/pro/massagetherapyapi/controller/RoleController.java
@@ -1,5 +1,6 @@
 package pavlo.pro.massagetherapyapi.controller;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pavlo.pro.massagetherapyapi.dto.RoleDto;
@@ -9,6 +10,8 @@ import pavlo.pro.massagetherapyapi.model.Role;
 import pavlo.pro.massagetherapyapi.service.interfaces.RoleService;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/role")
@@ -17,17 +20,25 @@ public class RoleController {
     @Autowired
     private RoleService roleService;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     @PostMapping("/create")
     public Response createRole(
         @RequestBody @Valid RoleDto roleDto
     ) {
         Role createdRole = roleService.createRole(roleDto);
+        modelMapper.map(createdRole, RoleDto.class);
         return Response.ok().setPayload(createdRole);
     }
 
     @GetMapping("")
     public Response getAllRoles() {
-        return Response.ok().setPayload(true);
+        List<Role> roles = roleService.getAllRoles();
+        List<RoleDto> rolesDto = roles.stream().map((role) -> {
+            return modelMapper.map(role, RoleDto.class);
+        }).collect(Collectors.toList());
+        return Response.ok().setPayload(rolesDto);
     }
 
     @GetMapping("/{name}")
