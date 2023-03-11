292f1f461ef9e6ecceca510b1fcc4efaebfc2840
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2023-03-11 12:58:40 +0100

    ST-59: One more attempt to make jasypt work with prod env

--- src/main/java/pavlo/pro/massagetherapyapi/MassageTherapyApiApplication.java
+++ src/main/java/pavlo/pro/massagetherapyapi/MassageTherapyApiApplication.java
@@ -1,9 +1,11 @@
 package pavlo.pro.massagetherapyapi;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class MassageTherapyApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MassageTherapyApiApplication.class, args);
@@ -13,4 +15,4 @@ public class MassageTherapyApiApplication {
 // Terminal commands
 // mvn spring-boot:run -Dspring-boot.run.profiles=dev
 // mvn jasypt:encrypt -Djasypt.encryptor.password=...
-//  mvn jasypt:encrypt -Djasypt.encryptor.password=buzhenitsa -Djasypt.plugin.path="file:src/main/resources/application-prod.properties"
+// mvn jasypt:encrypt -Djasypt.encryptor.password=buzhenitsa -Djasypt.plugin.path="file:src/main/resources/application-prod.properties"
